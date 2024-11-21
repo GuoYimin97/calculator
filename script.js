203e583f7895b332d8d870120de79ad45040fa9f
Yimin Guo <81876378+GuoYimin97@users.noreply.github.com>
2024-11-21 12:28:59 -0600

    finish almost the whole calculator proj, two minor things left

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ const division = function (a, b) {
 
 const operation = function (number1, number2, operator) {
     if (operator === "add") {
-        return add(number1, number2);
+        return Math.round(add(number1, number2) * 100) / 100;
     } else if (operator === "subtract") {
-        return subtract(number1, number2);
+        return Math.round(subtract(number1, number2) * 100) / 100;
     } else if (operator === "multiply") {
-        return multiply(number1, number2);
+        return Math.round(multiply(number1, number2) * 100) / 100;
     } else if (operator === "division") {
-        return division(number1, number2);
+        return Math.round(division(number1, number2) * 100) / 100;
     } else {
         return "Invalid Operator!"
     }
@@ -37,13 +37,14 @@ const operation = function (number1, number2, operator) {
 // ✓ operator buttons + - x /
 // ✓ equation button = which triggers operation function
 // ✓ clear button
+// ✓ populate the display when you click the digit buttons
+// ✓ clicking any operator (+ - x /) would store whatever you've entered as number1, display 1st number and the operator, then ready to read the next number 
+// ✓ click equal button would store whatever you've entered after clicking operator button as number2, then perform the calculation
+
+// ✓ if instead you clicked another operator after already clicking an operator, the mostly recently selected operator take precedence
+// 3 + 6 + 8 (clicking the second + will trigger the operation of 3 + 6)
 // backspace button
 // decimal button (once clicked, can't click again)
-// ✓ populate the display when you click the digit buttons
-
-// ✓ clicking any operator (+ - x /) would store whatever you've entered as number1, display 1st number and the operator, then ready to read the next number 
-// if instead you clicked another operator after already clicking an operator, the mostly recently selected operator take precedence
-// ✓ click equal button would store whatever you've entered after clicking operator button as number2, then perform the calculation
 
 
 let number1 = "";
@@ -52,12 +53,17 @@ let operator = "";
 let slctNumBtns = {};
 let slctOptBtns = {};
 let clckNumBtnsFnc = {};
+let slct2ndNumBtns = {};
+let clck2ndNumBtnsFnc = {};
+let firstOprClk = true;
+let clrFlag = false;
 const optArray = ["Add", "Sbtrct", "Mltp", "Dvsn"];
 frmlDsply = document.querySelector("#formula");
 ansDsply = document.querySelector("#answer");
 btnClr = document.querySelector("#btnClr");
+const btnEql = document.querySelector("#btnEql");
 
-const clrFncBrfOpr = function() {
+const clrFncBrfOpr = function () {
     number1 = "";
     number2 = "";
     operator = "";
@@ -80,67 +86,85 @@ for (let i = 0; i < 10; i++) {
     slctNumBtns[`btnNum${i}`].addEventListener("click", clckNumBtnsFnc[`read${i}`]);
 }
 
-const clkOprtn = function(event) {
+const clkOprtn = function (event) {
+
+    if (firstOprClk) {
+        firstOprClk = false;
+
+        if (event.target.id === "btnAdd") {
+            operator = "add";
+            frmlDsply.textContent = frmlDsply.textContent + " + ";
+        } else if (event.target.id === "btnSbtrct") {
+            operator = "subtract";
+            frmlDsply.textContent = frmlDsply.textContent + " - ";
+        } else if (event.target.id === "btnMltp") {
+            operator = "multiply";
+            frmlDsply.textContent = frmlDsply.textContent + " x ";
+        } else if (event.target.id === "btnDvsn") {
+            operator = "division";
+            frmlDsply.textContent = frmlDsply.textContent + " ÷ ";
+        }
     
-    if (event.target.id === "btnAdd") {
-        operator = "add";
-        frmlDsply.textContent = frmlDsply.textContent + " + ";
-    } else if (event.target.id === "btnSbtrct") {
-        operator = "subtract";
-        frmlDsply.textContent = frmlDsply.textContent + " - ";
-    } else if (event.target.id === "btnMltp") {
-        operator = "multiply";
-        frmlDsply.textContent = frmlDsply.textContent + " x ";
-    } else if (event.target.id === "btnDvsn") {
-        operator = "division";
-        frmlDsply.textContent = frmlDsply.textContent + " ÷ ";
-    }
-
-    let slct2ndNumBtns = {};
-    let clck2ndNumBtnsFnc = {};
-
-    for (let i = 0; i < 10; i++) {
-
-        slct2ndNumBtns[`btnNum${i}`] = document.querySelector(`#btnNum${i}`);
-
-        clck2ndNumBtnsFnc[`read${i}`] = function () {
-            if (operator !== "") {
-                number2 = number2 + `${i}`;
-                if (operator === "add") {
-                    frmlDsply.textContent = number1 + " + " + number2;
-                } else if (operator === "subtract") {
-                    frmlDsply.textContent = number1 + " - " + number2;
-                } else if (operator === "multiply") {
-                    frmlDsply.textContent = number1 + " x " + number2;
-                } else if (operator === "division") {
-                    frmlDsply.textContent = number1 + " ÷ " + number2;
+        slct2ndNumBtns = {};
+        clck2ndNumBtnsFnc = {};
+    
+        for (let i = 0; i < 10; i++) {
+    
+            slct2ndNumBtns[`btnNum${i}`] = document.querySelector(`#btnNum${i}`);
+    
+            clck2ndNumBtnsFnc[`read${i}`] = function () {
+                if (operator !== "") {
+                    number2 = number2 + `${i}`;
+                    if (operator === "add") {
+                        frmlDsply.textContent = number1 + " + " + number2;
+                    } else if (operator === "subtract") {
+                        frmlDsply.textContent = number1 + " - " + number2;
+                    } else if (operator === "multiply") {
+                        frmlDsply.textContent = number1 + " x " + number2;
+                    } else if (operator === "division") {
+                        frmlDsply.textContent = number1 + " ÷ " + number2;
+                    }
                 }
             }
+    
+            slct2ndNumBtns[`btnNum${i}`].addEventListener("click", clck2ndNumBtnsFnc[`read${i}`]);
+        }
+    
+        eqlFnc = function () {
+            ansDsply.textContent = operation(Number(number1), Number(number2), operator);
+        }
+        btnEql.addEventListener("click", eqlFnc);
+    
+        clrFnc = function () {
+            number1 = "";
+            number2 = "";
+            operator = "";
+            frmlDsply.textContent = "";
+            ansDsply.textContent = "";
+            for (let i = 0; i < 10; i++) {
+                slct2ndNumBtns[`btnNum${i}`].removeEventListener("click", clck2ndNumBtnsFnc[`read${i}`]);
+            }
+            firstOprClk = true;
+        }
+        btnClr.addEventListener("click", clrFnc);
+
+    } else {
+        if (event.target.id === "btnAdd") {
+            operator = "add";
+            frmlDsply.textContent = number1 + " + ";
+        } else if (event.target.id === "btnSbtrct") {
+            operator = "subtract";
+            frmlDsply.textContent = number1 + " - ";
+        } else if (event.target.id === "btnMltp") {
+            operator = "multiply";
+            frmlDsply.textContent = number1 + " x ";
+        } else if (event.target.id === "btnDvsn") {
+            operator = "division";
+            frmlDsply.textContent = number1 + " ÷ ";
         }
 
-        slct2ndNumBtns[`btnNum${i}`].addEventListener("click", clck2ndNumBtnsFnc[`read${i}`]);
     }
-
-    const btnEql = document.querySelector("#btnEql");
-    eqlFnc = function () {
-        ansDsply.textContent = operation(Number(number1), Number(number2), operator);
-    }
-    btnEql.addEventListener("click", eqlFnc);
-
-    clrFnc = function () {
-        number1 = "";
-        number2 = "";
-        operator = "";
-        frmlDsply.textContent = "";
-        ansDsply.textContent = "";
-
-        for (let i = 0; i < 10; i++) {
-            slct2ndNumBtns[`btnNum${i}`].removeEventListener("click", clck2ndNumBtnsFnc[`read${i}`]);
-        }
-
-    }
-
-    btnClr.addEventListener("click", clrFnc);
+    
 
 }
 
@@ -149,52 +173,3 @@ for (let i = 0; i < optArray.length; i++) {
     slctOptBtns[`btn` + optArray[i]].addEventListener("click", clkOprtn);
 }
 
-
-
-// const clkAdd = function () {
-//     operator = "add";
-//     frmlDsply.textContent = frmlDsply.textContent + " + ";
-
-//     let slct2ndNumBtns = {};
-//     let clck2ndNumBtnsFnc = {};
-
-//     for (let i = 0; i < 10; i++) {
-
-//         slct2ndNumBtns[`btnNum${i}`] = document.querySelector(`#btnNum${i}`);
-
-//         clck2ndNumBtnsFnc[`read${i}`] = function () {
-//             if (operator !== "") {
-//                 number2 = number2 + `${i}`;
-//                 console.log(number2);
-//                 console.log(clck2ndNumBtnsFnc[`read${i}`]);
-//                 frmlDsply.textContent = number1 + " + " + number2;
-//             }
-//         }
-
-//         slct2ndNumBtns[`btnNum${i}`].addEventListener("click", clck2ndNumBtnsFnc[`read${i}`]);
-//     }
-
-//     const btnEql = document.querySelector("#btnEql");
-//     eqlFnc = function () {
-//         ansDsply.textContent = operation(Number(number1), Number(number2), operator);
-//     }
-//     btnEql.addEventListener("click", eqlFnc);
-
-//     clrFnc = function () {
-//         number1 = "";
-//         number2 = "";
-//         operator = "";
-//         frmlDsply.textContent = "";
-//         ansDsply.textContent = "";
-
-//         for (let i = 0; i < 10; i++) {
-//             slct2ndNumBtns[`btnNum${i}`].removeEventListener("click", clck2ndNumBtnsFnc[`read${i}`]);
-//         }
-
-//     }
-
-//     btnClr.addEventListener("click", clrFnc);
-
-// }
-
-// slctOptBtns[`btnAdd`].addEventListener("click", clkAdd);
